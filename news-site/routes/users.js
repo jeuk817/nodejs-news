a7313b8efa95e28138d6ac187bd90dfd7d93596f
Ryujeuk <jeuk817@gmail.com>
2019-07-29 14:29:27 +0900

    feat: 로그인페이지 구현
    1. view/sign.pug로 로그인페이지 레이아웃을 구현
    2. view/login.pug에서 extends sign하고 로그인 페이지 구현
    3. public/stylesheets/sign.css에 css추가
    4. index.js에 url='/login' method=get 으로 login.pug 렌더링

--- news-site/routes/users.js
+++ news-site/routes/users.js
@@ -6,12 +6,6 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/flash', function (req, res, next) {
-  req.session.message = '세션 메시지';
-  req.flash('message', 'flash 메시지');
-  res.redirect('/users/flash/result');
-});
-
 router.get('/flash/result', function (req, res, next) {
   res.send(`${req.session.message} ${req.flash('message')}`);
 });

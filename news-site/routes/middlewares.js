461120a66edb056733147bb752324c55f3682846
Ryujeuk <jeuk817@gmail.com>
2019-08-18 23:55:28 +0900

    refactor: 미들웨어 isLoggedIn, isNotLoggedIn 수정
    세션과 토큰로그인 체크하는 함수들을 하나로 묶음.

--- news-site/routes/middlewares.js
+++ news-site/routes/middlewares.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken');
 // req.isAuthenticated()는 로그인상태면 true를 return합니다.
 
 // 로그인상태면 다음 미들웨어로 넘깁니다.
+// 1. session을 이용한 로그인확인 2. 토큰을 이용한 로그인 확인 3. 둘다 아니면 next()로 다음 미들웨어로 넘깁니다.
 exports.isLoggedIn = async (req, res, next) => {
     if (req.isAuthenticated()) {
         console.log('session login')
         return next();
     } else {
-        // res.render('homepage');
         try {
             const token = req.cookies.token;
             if (!token) {
@@ -34,36 +34,32 @@ exports.isLoggedIn = async (req, res, next) => {
 };
 
 // 로그아웃상태가 아니면 다음 미들웨어로 넘깁니다.
-// 로그인상태면 홈으로 redirect합니다.
-exports.isNotLoggedIn = (req, res, next) => {
+// 1. session 로그아웃상태인지 확인 2. 토큰로그아웃상태인지 확인 3. session,token 둘다 로그아웃 상태라면 next()를 하여 다음미들웨어로 넘깁니다.
+exports.isNotLoggedIn = async (req, res, next) => {
     if (!req.isAuthenticated()) {
-        return next();
-    } else {
-        res.redirect('/');
-    }
-}
+        try {
+            const token = req.cookies.token;
 
-// jwt토큰을 통해 로그인 되었는지 확인하는 함수.
-// 쿠키에 토큰이 저장되어있는지 확인후 없으면 홈페이지로, 있으면 req.userInfo에 담아서 next를 한다.
-exports.verifyToken = async (req, res, next) => {
-    try {
-        const token = req.cookies.token;
-        if (!token) {
-            return res.render('homepage');
-        }
-        const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
-        req.userInfo = decodedToken;
-        return next();
-    } catch (err) {
-        if (err.name === 'TokenExpiredError') {
-            return res.status(419).json({
-                code: 419,
-                message: '토큰이 만료되었습니다.'
+            if (!token) {
+                // session, token 둘다 로그아웃 상태일 때
+                return next();
+            }
+            // token 로그인 상태일 때
+            const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decodedToken;
+            return res.render('loginedhome', { user: req.user });
+        } catch (err) {
+            if (err.name === 'TokenExpiredError') {
+                // 토큰 유효기간이 지난 상태일 때
+                return next();
+            }
+            return res.status(401).json({
+                code: 401,
+                message: '유효하지 않은 토큰입니다.'
             });
         }
-        return res.status(401).json({
-            code: 401,
-            message: '유효하지 않은 토큰입니다.'
-        });
+    } else {
+        // session 로그인 상태일 때
+        return res.render('loginedhome', { user: req.user });
     }
 }
